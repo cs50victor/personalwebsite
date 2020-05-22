7e03c26ec17c07201440ac04c5a955c7a09eaef8
cs50victor <cs50victor@github.com>
2020-05-21 20:25:42 -0700

    improved typography

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import './App.scss';
 
 //----------Cool Animations and Preload-----------------------
 import {TweenLite, Power3} from "gsap";
-import ScrollMagic from 'scrollmagic';
+import ScrollMagic from "scrollmagic";
 //--------------Assets----------------------
-import Abi from "./Assets/abis.png";
+import css from "./Assets/techSvg/css-3.svg"
 import myface from "./Assets/face.png";
 import resume from "./Assets/Victor Atasie's Resume.pdf";
-
+import Abi from "./Assets/abis.png";
 
 export default function App(){
   const [isIconRotated, setIsIconRotated] = useState(false);
@@ -34,22 +34,21 @@ export default function App(){
     setIsIconRotated(isIconRotated => !isIconRotated)
   }
   //!--Current Year, GSAP and Scroll Magic--------
-  useEffect(()=>{
-    //!--Current Year---------
-    const yearEl = document.querySelector(".currentYear");
-    const date = new Date();
-    const year = date.getFullYear();
-    yearEl.innerText = year;
-
+  useEffect(()=>{ 
+    //!-- Header color and blur change on scroll
+    const header = document.querySelector(".fixed-top");
+    window.onscroll = () => {
+      if(window.scrollY >= 50.663) header.classList.add("appleBlur"); 
+      else header.classList.remove("appleBlur");
+    };
     //!--GSAP, TweenMax----
-    TweenLite.from(memojiText ,2,{opacity: 0,y: 0,ease: Power3.easeOut});
+    TweenLite.from(memojiText ,2,{opacity: 0,y: 70,ease: Power3.easeOut});
     introBox.scrollTop = 0;
 
     //!---ScrollMagic-----
     let scene1 = new ScrollMagic.Scene({
       triggerElement: introBox,
       triggerHook:"onLeave",
-      offset: -50,
       duration:"120%",
     })
     .setPin(introBox, {pushFollowers: false})
@@ -68,6 +67,12 @@ export default function App(){
     .addTo(controller)
 
     
+
+    //*--Current Year in Footer---------
+    const yearEl = document.querySelector(".currentYear");
+    const date = new Date();
+    const year = date.getFullYear();
+    yearEl.innerText = year;
   },[]);
 
   return (
@@ -110,7 +115,7 @@ export default function App(){
         </div>
       </header>
       <div className="content-wrapper"role="main">
-        <div className="intro1" 
+        <div className="introBox" 
          ref={el =>{introBox = el}} >
           <div className="intro text-center my-0 centerDown" ref={el =>{animBoxHead = el}} >
             <div ref={el => {memojiText = el}} className="appear1 mt-1">
@@ -124,7 +129,7 @@ export default function App(){
                 Resume
               </a>
               <br/>
-              <a className="text-white" href="#techs">
+              <a className="text-white" href="#moreMe">
                 <strong>Scroll for more</strong>
                 <br/>
                 <FontAwesomeIcon icon={faAngleDown} color={"white"} />
@@ -133,7 +138,7 @@ export default function App(){
           </div>
         </div>
         <div className="moreIntro center px-5"
-          ref={el => {aboutMe = el}}>
+          ref={el => {aboutMe = el}} id="moreMe">
             <div className="centerLittleText">
               <p> 
                 I'm a web designer / developer based in Toronto, Canada.
@@ -147,50 +152,50 @@ export default function App(){
           <div class="container">     
             <div class="row flex mx-2">
               <div class="col-12">
-                    <h3 className="my-3">Programming Languages</h3>
-                    <div className="box">
-                      <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
+                    <div className="box mt-5">
+                      <img className="img-thumbnail bg-white" src={css} alt="test"/>
                       <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                       <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                       <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                       <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     </div>     
-                    <h5 className="my-3">Programming Languages</h5>             
+                    <h5 className="mt-3">Programming Languages</h5> 
+                    <small>CSS</small>            
               </div>
               <div class="col-12">
-                  <h3 className="my-3">Libraries & Frameworks</h3>
-                  <div className="box">
+                  <div className="box mt-5">
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                   </div>
+                  <h5 className="my-3">Libraries and frameworks</h5>
               </div>
               <div class="col-12">
-                  <h3 className="my-3">Tools & Platforms</h3>
-                  <div className="box">
+                  <div className="box mt-5">
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                   </div>
+                  <h5 className="my-3">Tools & Platforms</h5>
               </div>
               <div class="col-12">
-                  <h3 className="my-3">Design</h3>
-                  <div className="box">
+                  <div className="box mt-5">
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                     <img className="img-fluid" src="//placehold.it/150x180" alt="test"/>
                   </div>
+                  <h5 className="my-3">Design</h5>
               </div>
             </div>
           </div>
         </div>
-        <div className="container projects mb-5 px-5" id="projects" ref={el => {projectsBox = el}}>
+        <div className="projects container my-5 px-5" id="projects" ref={el => {projectsBox = el}}>
           <hr/>
           <h3 className="ml-1">
            <kbd>Projects</kbd>
@@ -199,7 +204,7 @@ export default function App(){
           <div className="row mb-5 pb-md-4 align-items-center">
             <div className="col-md-5">
               <img className="img-fluid mt-3 mx-auto mb-5"
-                src="https://images.unsplash.com/photo-1588775286394-488d8cf1c8e9?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" 
+                src="https://images.pexels.com/photos/2101839/pexels-photo-2101839.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
                 alt="Random" loading="lazy" width="700" height="500"></img>
             </div>
             <div className="col-md-7 pl-md-5">
@@ -212,8 +217,8 @@ export default function App(){
                 Themes are built on Bootstrap as their own extended frameworks, 
                 rich with new components and plugins, documentation, and powerful build tools.
               </p>
-              <a href="#" className="btn btn-lg btn-outline-primary mb-3 mr-2">Live Demo</a>
-              <a href="#" className="btn btn-lg btn-outline-primary mb-3 mr-2">Source code on Github</a>
+              <a href="#" className="btn btn-md btn-outline-primary mb-3 mr-2">Live Demo</a>
+              <a href="#" className="btn btn-md btn-outline-primary mb-3 mr-2">Source code on Github</a>
             </div>
           </div>
           <div className="row mb-5 pb-md-4 align-items-center">
@@ -232,8 +237,8 @@ export default function App(){
                 Themes are built on Bootstrap as their own extended frameworks, 
                 rich with new components and plugins, documentation, and powerful build tools.
               </p>
-              <a href="#" className="btn btn-lg btn-outline-primary mb-3 mr-2">Live Demo</a>
-              <a href="#" className="btn btn-lg btn-outline-primary mb-3 mr-2">Source code on Github</a>
+              <a href="#" className="btn btn-md btn-outline-primary mb-3 mr-2">Live Demo</a>
+              <a href="#" className="btn btn-md btn-outline-primary mb-3 mr-2">Source code on Github</a>
             </div>
           </div>
           <div className="row mb-5 pb-md-4 align-items-center">
@@ -258,7 +263,7 @@ export default function App(){
           </div>
         </div>
       </div>
-      <footer className="text-center text-white py-4"><span>&#169;</span><span className="currentYear"></span> | Designed and Built by Victor Atasie</footer>
+      <footer className="text-center text-white py-4 mt-5"><span>&#169;</span><span className="currentYear"></span> | Designed and Built by Victor Atasie</footer>
     </React.Fragment>
   );
 }
