f3fa8cbe4f137290215a25fe51e09f734dfa145d
Victor (cs50Victor) <52110451+cs50victor@users.noreply.github.com>
2020-11-15 02:04:27 -0500

    fixing build issues

--- src/shared/Layout/Layout.jsx
+++ src/shared/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Container, Navbar} from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
